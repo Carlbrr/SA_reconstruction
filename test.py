6b0400f89d95fcfbe72b22349a69511639477115
Carl Bruun <carl99@live.dk>
2024-05-10 00:28:52 +0200

    Okay didnt commit for a while: added support for imports with levels... Also now catches decorators in ast Also found more issues.. : - not all imports are from top of file - somewhere we import just with ".." and write no module name

--- test.py
+++ test.py
@@ -3,8 +3,18 @@ from urllib import request
 import numpy as np
 #import ast 
 from zeeguu.config.loader import load_configuration_or_abort
+from ._only_teachers_decorator import only_teachers
 
 #print(os.getcwd())
 print(np.zeros((2, 2)))
 print(print(ast.zeros((2, 2))))
 print(load_configuration_or_abort())
+
+@api.route("/has_permission_for_cohort/<id>", methods=["GET"])
+@with_session
+@only_teachers
+def has_permission_for_cohort_api(id):
+
+    if has_permission_for_cohort(id):
+        return "OK"
+    return "Denied"

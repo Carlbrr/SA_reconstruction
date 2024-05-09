296fb4f7974b11a8cdbc8519edf0e88fa85ca43c
Carl Bruun <carl99@live.dk>
2024-05-09 14:04:50 +0200

    add project to git

---
+++ test.py
@@ -0,0 +1,10 @@
+import os
+from urllib import request
+import numpy as np
+import ast 
+from zeeguu.config.loader import load_configuration_or_abort
+
+#print(os.getcwd())
+print(np.zeros((2, 2)))
+print(print(ast.zeros((2, 2))))
+print(load_configuration_or_abort())
